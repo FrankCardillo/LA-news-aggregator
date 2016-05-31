3d6656f58695c08fe561861a7715fd67eca360c4
Frank Cardillo <frankcardillo@Franks-MacBook-Air.local>
2016-05-31 14:42:06 -0400

    Completed Basic and Bonus Requirements

--- server.rb
+++ server.rb
@@ -2,43 +2,48 @@ require "sinatra"
 require "pry"
 require "csv"
 
-# error InvalidSubmission do
-#   'So what happened was...' + env['sinatra.error'].message
-# end
-
 get "/articles/new" do
   erb :post_article
 end
 
-# post "/articles/new_post" do
-#   article_title = params['article_title']
-#   article_url = params['article_url']
-#   article_description = params['article_description']
-#
-#   if article_url[0..3] != "http"
-#     article_url = "http://" + article_url
-#   end
-#
-#   if article_title == nil || article_url == nil || article_description == nil
-#     raise InvalidSubmission, "you did not fill out all of the required fields."
-#     redirect '/articles/new'
-#   elsif article_description.length < 20
-#     raise InvalidSubmission, "you did not provide a description with a length of at least 20 characters."
-#     redirect '/articles/new'
-#   else
-#     CSV.foreach("articles.csv") do |row|
-#       if article_url == row[1]
-#         raise InvalidSubmission, "you are trying to submit an article that has previously been submitted."
-#       end
-#     end
-#
-#     File.open('articles.csv', 'a') do |file|
-#       file.puts(article_title + ", " + article_url + ", " + article_description)
-#     end
-#
-#     redirect '/articles'
-#   end
-# end
+post "/articles/new_post" do
+  @article_title = params['article_title']
+  @article_url = params['article_url']
+  @article_description = params['article_description']
+  @error_msg
+  dupe_article = false
+
+  if @article_url[0..3] != "http"
+    @article_url = "http://" + @article_url
+  end
+
+  CSV.foreach("articles.csv") do |row|
+    if @article_url == row[1]
+      dupe_article = true
+      break
+    end
+  end
+  if @article_title.length == 0 || @article_url.length == 0 || @article_description.length == 0
+    @error_msg = "You did not supply all arguments"
+    erb :post_article
+  elsif @article_description.length < 20
+    @error_msg = "Your description is < 20 chars long."
+    erb :post_article
+  else
+    if dupe_article
+      @error_msg = "That article has already been submitted"
+      erb :post_article
+    else
+      @error_msg = ""
+
+      File.open('articles.csv', 'a') do |file|
+        file.puts(@article_title + "," + @article_url + "," + @article_description)
+      end
+
+      redirect '/articles'
+    end
+  end
+end
 
 get "/articles" do
   @titles = []
